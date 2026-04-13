4ec2d3149548096ea4993091f2923eb461cf6266
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-04-13 22:08:47 +0800

    halaman detail program

--- app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/AddProgram.kt
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/AddProgram.kt
@@ -1,6 +1,8 @@
 package com.yourname.aplikasitrackingpendayagunaan
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,15 @@ class AddProgram : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+        val options = listOf("UPZPRENEUR", "Gerobak Berkah" , "Z-Mart" , "Z-Auto" )
+        val adapter = ArrayAdapter(this, R.layout.list_item, options)
+
+        val autoComplete  = findViewById<AutoCompleteTextView>(R.id.autoComplete)
+        autoComplete.setAdapter(adapter)
     }
+
+
 }
